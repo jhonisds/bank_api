489227a18a105064a190e7de0795463338e83d5a
Jhoni Santos <jhoni.sousa@fatec.sp.gov.br>
2020-07-24 19:30:21 -0300

    add fields shema users.

--- lib/bank_api/accounts/user.ex
+++ lib/bank_api/accounts/user.ex
@@ -4,4 +4,19 @@ defmodule BankApi.Accounts.User do
   """
   use Ecto.Schema
   import Ecto.Changeset
+
+  schema "users" do
+    field :email, :string
+    field :name, :string
+    field :last_name, :string
+    field :password, :string, virtual: true
+    field :password_confirmation, :string, virtual: true
+    field :password_hash, :string
+    field :role, :string
+
+    timestamps()
+  end
+
+  def changeset() do
+  end
 end
