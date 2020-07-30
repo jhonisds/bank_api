1dc0e029469bd1406dbe6cd4eff60c425635cfd3
Jhoni Santos <jhoni.sousa@fatec.sp.gov.br>
2020-07-30 20:58:08 -0300

    add operations sub and sum to the context.

--- lib/bank_api/operations.ex
+++ lib/bank_api/operations.ex
@@ -8,5 +8,44 @@ defmodule BankApi.Operations do
 
   def transfer(f_id, t_id, value) do
     from = Accounts.get!(f_id)
+    value = Decimal.new(value)
+
+    case is_negative?(from.balance, value) do
+      true ->
+        {:error, "you can't have negative balance"}
+
+      false ->
+        perform_update(from, t_id, value)
+    end
+  end
+
+  def is_negative?(from_balance, value) do
+    Decimal.sub(from_balance, value)
+    |> Decimal.negative?()
+  end
+
+  def perform_update(from, t_id, value) do
+    {:ok, from} = perform_operation(from, value, :sub)
+
+    {:ok, to} =
+      Accounts.get!(t_id)
+      |> perform_operation(value, :sum)
+
+    {:ok, "Transfer with success! from: #{from.id} to: #{to.id} value: #{value}"}
+  end
+
+  def perform_operation(account, value, :sum) do
+    account
+    |> update_account(%{balance: Decimal.add(account.balance, value)})
+  end
+
+  def perform_operation(account, value, :sub) do
+    account
+    |> update_account(%{balance: Decimal.sub(account.balance, value)})
+  end
+
+  def update_account(%Account{} = account, attrs) do
+    Account.changeset(account, attrs)
+    |> Repo.update!()
   end
 end
